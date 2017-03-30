0b06d8333f020ad24f63bf2f70959787125b2b37
Michael Goodness <mgoodness@gmail.com>
2017-03-29 22:06:10 -0500

    Show current AWS profile in right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,11 +1,18 @@
+function __get_aws_profile
+  if test -n "$aws_profile"
+    echo -n "[$aws_profile]"
+  end
+end
+
 function __get_k8s_context
   if test -n (kubectl config current-context)
-    echo -e -n -s $k8s_color "($k8s_context)"
+    echo -n "($k8s_context)"
   end
 end
 
 function fish_right_prompt
   set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
+  __get_aws_profile
   __get_k8s_context
   set_color normal
 end
