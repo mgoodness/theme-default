c53f7d8a393f6693fe631d45a9b83d2f2723d313
Michael Goodness <mgoodness@gmail.com>
2017-03-29 23:44:12 -0500

    Customized

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,57 +1,36 @@
 # You can override some default options with config.fish:
 #
-#  set -g theme_short_path yes
+#  set -g theme_short_path
 
 function fish_prompt
-  set -l last_command_status $status
-  set -l cwd
+  set -l last_status $status
 
-  if test "$theme_short_path" = 'yes'
-    set cwd (basename (prompt_pwd))
-  else
-    set cwd (prompt_pwd)
-  end
-
-  set -l fish     "⋊>"
   set -l ahead    "↑"
   set -l behind   "↓"
   set -l diverged "⥄ "
-  set -l dirty    "⨯"
-  set -l none     "◦"
+  set -l fish     (test (uname) = Darwin; and echo "⋊>"; or echo "⋊ >")
 
+  set -l clean_color      (set color green)
+  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red)
   set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress ^/dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error ^/dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote ^/dev/null; or set_color brown)
-  set -l repository_color (set_color $fish_color_cwd ^/dev/null; or set_color green)
-
-  if test $last_command_status -eq 0
-    echo -n -s $success_color $fish $normal_color
-  else
-    echo -n -s $error_color $fish $normal_color
-  end
+  set -l touched_color    (set_color yellow)
 
   if git_is_repo
-    if test "$theme_short_path" = 'yes'
-      set root_folder (command git rev-parse --show-toplevel ^/dev/null)
-      set parent_root_folder (dirname $root_folder)
-      set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-
-      echo -n -s " " $directory_color $cwd $normal_color
-    else
-      echo -n -s " " $directory_color $cwd $normal_color
-    end
-
-    echo -n -s " on " $repository_color (git_branch_name) $normal_color " "
-
+    set -l branch_color $clean_color
     if git_is_touched
-      echo -n -s $dirty
-    else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
+      set branch_color $touched_color
     end
-  else
-    echo -n -s " " $directory_color $cwd $normal_color
+
+    echo -n -s $normal_color ' · '
+    echo -n -s '(' $branch_color (git_branch_name)
+    echo -n -s (git_ahead $ahead $behind $diverged $none)
+    echo -n -s $normal_color ')'
   end
 
-  echo -n -s " "
+  set -l prompt_color $error_color
+  if test $last_status = 0
+    set prompt_color $normal_color
+  end
+
+  echo -n -s $prompt_color " $fish "
 end
