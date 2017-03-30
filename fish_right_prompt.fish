92160e2ff7d28bd54673c305ca5e4b3b22506b86
Michael Goodness <mgoodness@gmail.com>
2017-03-29 22:01:58 -0500

    Show current Kubernetes context in right prompt

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,5 +1,11 @@
+function __get_k8s_context
+  if test -n (kubectl config current-context)
+    echo -e -n -s $k8s_color "($k8s_context)"
+  end
+end
+
 function fish_right_prompt
   set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
-  date "+%H:%M:%S"
+  __get_k8s_context
   set_color normal
 end
